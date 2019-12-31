362b1df91a6eaa1e17bcbc79a589d9f1022dede0
moayad <Moayad>
2020-01-01 00:33:08 +0200

    add about text

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const path=require('path');
 const express=require('express');
 const app=express();
-const port=process.env.PORT||8000;
+const port=process.env.PORT||8080;
 const hbs=require("hbs");
 const forecast=require("./utils/forecast");
 const geocode=require("./utils/geocode");
