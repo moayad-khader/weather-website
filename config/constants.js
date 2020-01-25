ae36b768a1fa83a88ee4b8a782821175f60292cc
Ibrahim shamma <i.abushammah@gmail.com>
2020-01-25 10:56:14 +0200

    fix routes

---
+++ config/constants.js
@@ -0,0 +1,11 @@
+const Routes = {
+    base: '/',
+    public: '../public',
+    views: '../templates/views',
+    partials: '../templates/partials',
+    about: "/about",
+    help: '/help',
+    weather: '/weather',
+    help: '/help/*'
+};
+module.exports = Routes;
--- src/app.js
+++ src/app.js
@@ -1,94 +1,94 @@
-const path=require('path');
-const express=require('express');
-const app=express();
-const port=process.env.PORT||8080;
-const hbs=require("hbs");
-const forecast=require("./utils/forecast");
-const geocode=require("./utils/geocode");
-
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const hbs = require("hbs");
+const forecast = require("./utils/forecast");
+const geocode = require("./utils/geocode");
+const Routes = require("../config/constants");
 //define paths for express
-const public_directory=path.join(__dirname,'../public');
-const views_directory=path.join(__dirname,'../templates/views');
-const partials_directory=path.join(__dirname,'../templates/partials');
+const public_directory = path.join(__dirname, Routes.public);
+const views_directory = path.join(__dirname, Routes.views);
+const partials_directory = path.join(__dirname, Routes.partials);
 
 //set up handelbars engine and views location
-app.set('view engine','hbs');
-app.set('views',views_directory);
+app.set('view engine', 'hbs');
+app.set('views', views_directory);
 hbs.registerPartials(partials_directory);
 
 //set up static directory to serve
 app.use(express.static(public_directory));
 
-app.get('',(req,res)=>{
-    res.render("index",{
-        'title':'Weather APP',
-        'name':'Moayad Khader'
+app.get(Routes.base, (req, res) => {
+    res.render("index", {
+        'title': 'Weather APP',
+        'name': 'Moayad Khader'
     })
 });
 
-app.get('/about',(req,res)=>{
-    res.render("about",{
-        'title':"About",
-        "picture":"/img/me.jpg",
-        "name":"Moayad Khader"
+app.get(Routes.about, (req, res) => {
+    res.render("about", {
+        'title': "About",
+        "picture": "/img/me.jpg",
+        "name": "Moayad Khader"
     });
 });
 
-app.get('/help',(req,res)=>{
-    res.render("help",{
-        "title":"Help page",
-        "message":"do you face any problem?",
-        "name":"Moayad Khader"
+app.get(Routes.help, (req, res) => {
+    res.render("help", {
+        "title": "Help page",
+        "message": "do you face any problem?",
+        "name": "Moayad Khader"
     });
 });
 
-app.get('/weather',(req,res)=>{
-    if(!req.query.address){
+app.get(Routes.weather, (req, res) => {
+    if (!req.query.address) {
         return res.send({
-            "error":"you must provide an address"
+            "error": "you must provide an address"
         })
     }
 
-    geocode(req.query.address,(geocode_error,{latitude,longitude,location}={})=>{
-        if(geocode_error){
+    geocode(req.query.address, (geocode_error, { latitude, longitude, location } = {}) => {
+        if (geocode_error) {
             return res.send({
-                "error":geocode_error
+                "error": geocode_error
             });
         }
 
-        forecast(latitude,longitude,(forecast_error,forecast_data)=>{
-            if(forecast_error){
-                return res,send({
-                    "error":forecast_error
+        forecast(latitude, longitude, (forecast_error, forecast_data) => {
+            if (forecast_error) {
+                return res, send({
+                    "error": forecast_error
                 });
             }
             res.send({
-                location:location,
-                forecast:forecast_data,
-                address:req.query.address
+                location: location,
+                forecast: forecast_data,
+                address: req.query.address
             });
-            
+
         })
     });
 
 });
 
-app.get('/help/*',(req,res)=>{
-    res.render('404_handler',{
-        'title':'Error',
-        'name':'Moayad Khader',
-        'error_message':'help article not found'
+app.get(Routes.help, (req, res) => {
+    res.render('404_handler', {
+        'title': 'Error',
+        'name': 'Moayad Khader',
+        'error_message': 'help article not found'
     })
 })
 
-app.get("*",(req,res)=>{
-    res.render('404_handler',{
-        'title':'Error',
-        'name':'Moayad Khader',
-        'error_message':'page not found'
+app.get("*", (req, res) => {
+    res.render('404_handler', {
+        'title': 'Error',
+        'name': 'Moayad Khader',
+        'error_message': 'page not found'
     });
 });
- 
-app.listen(port,()=>{
-    console.log("Server is up on port "+port);
+
+app.listen(port, () => {
+    console.log("Server is up on port " + port);
 });
